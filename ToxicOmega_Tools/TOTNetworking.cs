61f633c12c066d37955fb97a6fd85bd64c7643d8
Joshua Remlinger <pickichu@icloud.com>
2024-01-04 21:04:16 -0500

    Small security update to make sure clients don't accept RPCs unless it is from the host

--- ToxicOmega_Tools/TOTNetworking.cs
+++ ToxicOmega_Tools/TOTNetworking.cs
@@ -12,14 +12,29 @@ namespace ToxicOmega_Tools.Patches
 {
     internal class TOTNetworking
     {
+        private static string hostVerifiedMessage = "RPC SENDER VERIFIED AS HOST, PROCEEDING WITH HANDLER METHOD.";
+        private static string nonHostSenderMessage = "RPC SENDER IS NOT THE HOST, HANDLER METHOD CANCELLED.";
+
         [NetworkMessage("TOT_CHARGE_PLAYER", true)]
         public static void TOT_CHARGE_PLAYER_HANDLER(ulong sender, TOT_PLAYER_Broadcast message)
         {
             Plugin.mls.LogInfo("RPC RECEIVED: \"TOT_CHARGE_PLAYER\".");
 
-            PlayerControllerB playerTarget = StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(message.playerClientId));
-            GrabbableObject foundItem = playerTarget.ItemSlots[playerTarget.currentItemSlot];
-            foundItem.insertedBattery.charge = 1f;
+            if (StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(sender)).IsHost)
+            {
+                Plugin.mls.LogInfo(hostVerifiedMessage);
+                PlayerControllerB playerTarget = StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(message.playerClientId));
+                GrabbableObject foundItem = playerTarget.ItemSlots[playerTarget.currentItemSlot];
+
+                if (foundItem != null && foundItem.itemProperties.requiresBattery)
+                {
+                    foundItem.insertedBattery.charge = 1f;
+                }
+            }
+            else
+            {
+                Plugin.mls.LogInfo(nonHostSenderMessage);
+            }
         }
 
         [NetworkMessage("TOT_HEAL_PLAYER", true)]
@@ -27,13 +42,21 @@ namespace ToxicOmega_Tools.Patches
         {
             Plugin.mls.LogInfo("RPC RECEIVED: \"TOT_HEAL_PLAYER\".");
 
-            PlayerControllerB playerTarget = StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(message.playerClientId));
-
-            if (playerTarget != null)
+            if (StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(sender)).IsHost)
             {
-                Plugin.SavePlayer(playerTarget);
-                playerTarget.isExhausted = false;
-                playerTarget.bleedingHeavily = false;
+                Plugin.mls.LogInfo(hostVerifiedMessage);
+                PlayerControllerB playerTarget = StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(message.playerClientId));
+
+                if (playerTarget != null)
+                {
+                    Plugin.SavePlayer(playerTarget);
+                    playerTarget.isExhausted = false;
+                    playerTarget.bleedingHeavily = false;
+                }
+            }
+            else
+            {
+                Plugin.mls.LogInfo(nonHostSenderMessage);
             }
         }
 
@@ -49,14 +72,23 @@ namespace ToxicOmega_Tools.Patches
         {
             Plugin.mls.LogInfo("RPC RECEIVED: \"TOT_TERMINAL_CREDITS\".");
 
-            Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
-            if (terminal != null)
+            if (StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(sender)).IsHost)
             {
-                terminal.groupCredits += message.dataInt;
+                Plugin.mls.LogInfo(hostVerifiedMessage);
+                Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+
+                if (terminal != null)
+                {
+                    terminal.groupCredits += message.dataInt;
+                }
+                else
+                {
+                    Plugin.LogMessage("Terminal not found!", true);
+                }
             }
             else
             {
-                Plugin.LogMessage("Terminal not found!", true);
+                Plugin.mls.LogInfo(nonHostSenderMessage);
             }
         }
 
@@ -64,14 +96,32 @@ namespace ToxicOmega_Tools.Patches
         public static void TOT_TP_PLAYER_HANDLER(ulong sender, TOT_TP_PLAYER_Broadcast message)
         {
             Plugin.mls.LogInfo("RPC RECEIVED: \"TOT_TP_PLAYER\".");
-            Plugin.PlayerTeleportEffects(StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(message.playerClientId)), message.isInside);
+
+            if (StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(sender)).IsHost)
+            {
+                Plugin.mls.LogInfo(hostVerifiedMessage);
+                Plugin.PlayerTeleportEffects(StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(message.playerClientId)), message.isInside);
+            }
+            else
+            {
+                Plugin.mls.LogInfo(nonHostSenderMessage);
+            }
         }
 
         [NetworkMessage("TOT_SYNC_AMMO", true)]
         public static void TOT_SYNC_AMMO_HANDLER(ulong sender, TOT_ITEM_Broadcast message)
         {
             Plugin.mls.LogInfo("RPC RECEIVED: \"TOT_SYNC_AMMO\".");
-            LC_API.GameInterfaceAPI.Features.Item.List.FirstOrDefault(item => item.NetworkObjectId.Equals(message.networkObjectID)).GetComponentInChildren<ShotgunItem>().shellsLoaded = 2;
+
+            if (StartOfRound.Instance.allPlayerScripts.FirstOrDefault(player => player.playerClientId.Equals(sender)).IsHost)
+            {
+                Plugin.mls.LogInfo(hostVerifiedMessage);
+                LC_API.GameInterfaceAPI.Features.Item.List.FirstOrDefault(item => item.NetworkObjectId.Equals(message.networkObjectID)).GetComponentInChildren<ShotgunItem>().shellsLoaded = 2;
+            }
+            else
+            {
+                Plugin.mls.LogInfo(nonHostSenderMessage);
+            }
         }
     }
 
